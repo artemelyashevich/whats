e590775046042b394e044cfd076bb3fb5fc8cb0c
Artem-Elyashevich <elyashevich.artem06@gmail.com>
2024-12-31 15:51:53 +0300

    Initial commit

---
+++ src/main/java/com/elyashevich/whats_clone/WhatsCloneApplication.java
@@ -0,0 +1,13 @@
+package com.elyashevich.whats_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elyashevich/whats_clone/WhatsCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elyashevich.whats_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
